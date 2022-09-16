1c8c91190c46751c71b1d0504904b95606426fd9
Wolfgang Marbler <52210474@edu.campus02.at>
2022-09-16 19:13:00 +0200

    inital commit

---
+++ src/Helloworldkonfg.java
@@ -0,0 +1,12 @@
+public class Helloworldkonfg {
+    //psvm oder main + TAB
+    public static void main(String[] args) {
+        //sout + Tab
+        System.out.println("Hello World");
+        System.out.println("Welcome to KONF 2022");
+        System.out.println("Zeile 3");
+        System.out.println("Zeile 4"); //STRG+D
+    }
+}
+
+
